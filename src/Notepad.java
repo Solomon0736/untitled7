9e66f4dac236edd7316e83f68673c4da9e039568
solomontikabo.musie <Seli1230>
2020-03-23 17:50:53 +0100

    ändringar1

--- src/Notepad.java
+++ src/Notepad.java
@@ -1,18 +1,55 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class Notepad {
     private JButton clear;
-    private JButton stäng;
+    private JPanel start;
+    private JTextArea background;
+    private JButton Start;
+    private JButton copy;
     private JButton home;
 
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Notepad");
+        frame.setContentPane(new Notepad().start);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        //this.setLoctionRelativeTo( nu)
+    }
 
     public Notepad() {
-        stäng.addActionListener(new ActionListener() {
+        clear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+              clear.setText(null);
+            }
+        });
+
+        home.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent actionEvent) {
 
             }
         });
+        copy.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        background.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent componentEvent) {
+                super.componentResized(componentEvent);
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
